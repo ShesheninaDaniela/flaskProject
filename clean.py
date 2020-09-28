d79c7ad60a0bc6d8517b955280bbc8a88d6c23f9
ShesheninaDaniela <36850289+ShesheninaDaniela@users.noreply.github.com>
2020-09-28 22:03:33 +0500

    1

--- clean.py
+++ clean.py
@@ -3,5 +3,7 @@ import os
 clean_container = 'docker rm -f `docker ps -a | grep \'test_calculator\' | awk \'{print $1;}\'`'
 print(str(os.system(clean_container)))
 
+
 clean_untagged_image = 'docker rmi `docker images | grep \'test_calculator\' | awk \'{ print $3; }\'`'
+
 print(str(os.system(clean_untagged_image)))
