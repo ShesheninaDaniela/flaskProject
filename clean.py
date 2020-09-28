8ffe5dbb38b9e315a305205474c4f4269c636980
ShesheninaDaniela <36850289+ShesheninaDaniela@users.noreply.github.com>
2020-09-28 21:23:48 +0500

    1

--- clean.py
+++ clean.py
@@ -1,7 +1,7 @@
 import os
 
-clean_container = 'docker rm -f `docker ps -a | grep \'test_calculate\' | awk \'{print $1;}\'`'
+clean_container = 'docker rm -f `docker ps -a | grep \'test_calculator\' | awk \'{print $1;}\'`'
 print(str(os.system(clean_container)))
 
-clean_untagged_image = 'docker rmi `docker images | grep \'test_calculate\' | awk \'{ print $3; }\'`'
+clean_untagged_image = 'docker rmi `docker images | grep \'test_calculator\' | awk \'{ print $3; }\'`'
 print(str(os.system(clean_untagged_image)))
